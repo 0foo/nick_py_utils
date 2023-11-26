0d71cdaba030690960c4b414ee4b7821009b0582
0foo <0foo@0foo.dev>
2023-11-26 12:19:01 -0800

    upd

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name='nickutils',
+    version='0.0.1',
+    author='Nick Kiermaier',
+    author_email='nick@nickk.info',
+    description='Collection of python utilities',
+    long_description_content_type="text/markdown",
+    url='https://github.com/mike-huls/toolbox',
+    project_urls = {
+        "Bug Tracker": ""
+    },
+    license='MIT',
+    packages=['modules'],
+    install_requires=[],
+)
